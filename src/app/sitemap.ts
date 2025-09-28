46533b34f478e69754a71e019607bf954f3a60ec
Abubeker Abduljelil <114282920+corax58@users.noreply.github.com>
2025-09-28 15:55:48 +0300

    feat: update sitemap (#8)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,13 +18,61 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://growztech.com/projects",
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.5,
+      priority: 0.8,
     },
     {
       url: "https://growztech.com/blogs",
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.5,
+      priority: 0.8,
+    },
+    {
+      url: "https://www.growztech.com/projects/taza-house",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.growztech.com/projects/billion-views-ai",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.growztech.com/projects/study-nest-app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.growztech.com/blogs/mobile-vs-web-apps-which-one-does-your-business-need",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: "https://www.growztech.com/blogs/why-we-choose-this-career-understanding-software-development",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: "https://www.growztech.com/blogs/the-impact-of-ui-ux-on-digital-success",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: "https://www.growztech.com/blogs/the-role-of-ai-in-modern-software-development",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: "https://www.growztech.com/blogs/the-role-of-it-consultation-in-business-growth",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
     },
   ];
 }
