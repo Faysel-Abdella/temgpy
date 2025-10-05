3cce16de47baf7b3b2ade4c39c225785c3201e93
Abubeker Abduljelil <Abubekera87@gmail.com>
2025-10-05 20:15:37 +0300

    feat: add faq page cta

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,13 +1,14 @@
 import FaqAccordionItem from "@/components/faq-accordion-item";
 import SectionShow from "@/components/section-show";
 import { Accordion } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 import { faqContent } from "@/data/faq";
 import React from "react";
 
 const FAQPage = () => {
   return (
-    <section className=" px-4 py-28 md:py-32 lg:py-40 bg-gradient-to-b from-background3 to-white min-h-dvh overflow-hidden">
-      <div className="container mx-auto max-w-7xl">
+    <section className="  pt-28 md:pt-32 lg:pt-40 bg-gradient-to-b from-background3 to-white min-h-dvh overflow-hidden">
+      <div className="container mx-auto max-w-7xl px-4">
         <div className="mx-auto  max-w-3xl text-center">
           <SectionShow title="FAQs" />
           <h2
@@ -46,6 +47,25 @@ const FAQPage = () => {
             })}
           </Accordion>
         </div>
+      </div>
+      <div className="bg-muted mt-24 flex items-center flex-col justify-center w-full py-24 px-4">
+        <h2
+          data-aos="fade-up"
+          data-aos-duration="600"
+          className="mt-6  text-2xl  font-extrabold text-center font-gilroy text-foreground md:text-4xl lg:text-5xl"
+        >
+          Have any Other Questions?
+        </h2>
+        <p
+          data-aos="fade-up"
+          data-aos-duration="700"
+          className="mt-4 text-base text-description md:text-lg max-w-md text-center "
+        >
+          Don&apos;t Hesitate to contact us with your enquiry or statement.
+        </p>
+        <Button className="rounded-full mt-8 text-base py-6 px-8" size={"lg"}>
+          Book A Free Call
+        </Button>
       </div>
     </section>
   );
