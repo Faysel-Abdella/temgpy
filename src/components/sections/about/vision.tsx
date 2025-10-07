81fd3eedcdd4a7b9b67dd04c70ec1adb400e6a42
Faysel Abdella <fayselcode@gmail.com>
2025-10-07 15:44:20 +0300

    update spaces

--- src/components/sections/about/vision.tsx
+++ src/components/sections/about/vision.tsx
@@ -7,12 +7,13 @@ import React from "react";
 
 export default function Vision() {
   return (
-    <main className="w-screen h-fit flex items-center justify-center py-20 ">
+    <main className="w-screen h-fit flex items-center justify-center py-20 max-md:py-2 ">
       <div className="w-full p-4  lg:max-w-[1232px] items-start h-fit flex flex-col  ">
         <div className="flex flex-col w-full items-center gap-8 md:gap-12 lg:gap-16">
           <SectionShow title="Our Vision" />
           <p className="w-full text-center  text-3xl  justify-start  md:text-4xl font-extrabold font-gilroy leading-[40px]">
-            We believe in transforming ideas into powerful digital experience
+            To be global leaders in delivering innovative technology solutions
+            that help businesses grow.{" "}
           </p>
         </div>
         <Separator className="w-full hidden md:block my-12" />
@@ -21,32 +22,32 @@ export default function Vision() {
             <div className="flex max-md:items-center flex-col gap-4">
               <Icons.MedalStar className="" />
               <div className=" mt-5 justify-start  text-xl font-semibold font-gilroy ">
-                Empowering Digital Growth
+                Global Excellence{" "}
               </div>
               <div className="max-md:max-w-xs max-md:mx-auto justify-start max-md:text-center text-description text-base font-medium font-inter leading-normal">
-                We aim to enable Businesses to thrive online by delivering
-                tailored software solutions that lead to measurable results
+                Our aim is to be a worldwide technology leader, ensuring you get
+                software built on the highest global standards.
               </div>
             </div>
 
             <div className="flex max-md:items-center  flex-col gap-4">
               <Icons.Bulb className="" />
               <div className=" justify-start mt-5 text-xl font-semibold font-gilroy ">
-                Driving Innovation
+                Innovation-focused{" "}
               </div>
               <div className="max-md:max-w-xs max-md:mx-auto justify-start max-md:text-center text-description text-base font-medium font-inter leading-normal">
-                We Integrate latest technologies and innovative strategies to
-                ensure our clients stays ahead of competition in digital world.
+                We pioneer new solutions and apply forward-thinking technology
+                to solve your toughest business challenges.
               </div>
             </div>
             <div className="flex  max-md:items-center flex-col gap-4">
               <Icons.Chart className="" />
               <div className=" justify-start mt-5 text-xl font-semibold font-gilroy ">
-                Driving Innovation
+                Helping businesses grow{" "}
               </div>
               <div className="max-md:max-w-xs max-md:mx-auto justify-start max-md:text-center text-description text-base font-medium font-inter leading-normal">
-                We Integrate latest technologies and innovative strategies to
-                ensure our clients stays ahead of competition in digital world.
+                We focus on a clear plan to support our clients in achieving
+                meaningful, long-term growth and success.
               </div>
             </div>
           </div>
