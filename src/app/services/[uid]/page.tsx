815e8747cde74f3fff8199db0dc533505d07a668
Abubeker Abduljelil <Abubekera87@gmail.com>
2025-10-11 18:09:39 +0300

    feat : add navigation buttons to the other services carousel

--- src/app/services/[uid]/page.tsx
+++ src/app/services/[uid]/page.tsx
@@ -140,7 +140,7 @@ const ServiceDetailPage = async ({ params }: ServiceDetailPageProps) => {
         <div className="flex bg-white">
           <Separator className="mx-auto max-w-6xl " />
         </div>
-        <div className="max-w-6xl mx-auto px-4">
+        <div className="max-w-6xl mx-auto px-4 sm:px-12">
           <OtherServices currentServiceId={uid} />
         </div>
       </div>
