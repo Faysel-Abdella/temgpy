dfebd95b923574a1b0781692a3fe6dd3d50a1f7c
Abubeker Abduljelil <114282920+corax58@users.noreply.github.com>
2025-10-22 20:59:54 +0300

    Abubeker/feat/update logo (#25)
    * feat: update navbar logo dimensions
    
    * feat: update logo
    
    * feat: update sitemap.ts with the newest content

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,3 +1,4 @@
+import { services } from "@/data/services";
 import { createClient } from "@/prismicio";
 import type { MetadataRoute } from "next";
 
@@ -7,6 +8,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     process.env.NEXT_PUBLIC_BASE_URL || "https://venastechnology.com";
 
   const blogs = await client.getAllByType("blog");
+
+  const serviceUrls = services.map((service, index) => ({
+    url: `${baseUrl}/services/${service.id}`,
+    lastModified: new Date(),
+    priority: index > 3 ? 0.64 : 0.8,
+  }));
+
   const postUrls = blogs.map((post) => ({
     url: `${baseUrl}/blogs/${post.uid}`,
     lastModified: new Date(post.last_publication_date),
@@ -26,12 +34,31 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.8,
     },
     {
+      url: "https://venastechnology.com/contact-us",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://venastechnology.com/faq",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: "https://venastechnology.com/blogs",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://venastechnology.com/services",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
 
     ...postUrls,
+    ...serviceUrls,
   ];
 }
