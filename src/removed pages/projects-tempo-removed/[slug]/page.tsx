6c76e1c089f86a7a0bc3dfd2d437e63fd992af3c
yusra reshid <yusramuhaba8@gmail.com>
2026-01-14 15:16:43 +0300

    Handled

--- src/removed pages/projects-tempo-removed/[slug]/page.tsx
+++ src/removed pages/projects-tempo-removed/[slug]/page.tsx
@@ -9,7 +9,6 @@ import {
 } from "@/components/sections/works/projects-data";
 import { Separator } from "@/components/ui/separator";
 import TechStacks from "@/components/sections/works/project-details/tech-stacks";
-import { redirect } from "next/navigation";
 
 // Generate metadata for each project dynamically
 export async function generateMetadata({
@@ -77,21 +76,9 @@ export default async function Page({
   const project: Project | undefined = getOneProject(slug);
   // console.log("projects , ", project);
 
-  // if (!project) {
-  //   return <div>Project not found</div>;
-  // }
-
-  const removedProjects = [
-  "billion-views-ai",
-  "taza-house",
-  "study-nest-app",
-];
-
-
-  if (!project || removedProjects.includes(slug)) {
-  redirect("/projects"); 
-}
-
+  if (!project) {
+    return <div>Project not found</div>;
+  }
 
   const projects = getManyProjects();
   // console.log("projects , ", projects);
